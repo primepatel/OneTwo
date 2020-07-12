36eee7ea200fc3057bd2403629924406fd9503e5
Manish <primespatel@gmail.com>
2020-07-12 09:36:48 +0530

    added functionality custom Limit Value

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ print("welcome")
 player_1 = input("Player 1 Name: ")
 player_2 = input("Player 2 Name: ")
 
-limit_value = 10
+limit_value = int(input("Limit Value: "))
+
 current_sum = 0
 
 for i in range(limit_value):
     player_1_num = int(input(player_1 + ": "))
     current_sum += player_1_num
-    print("Current sum: ", current_sum)
+    print("Current sum: ", current_sum, " Limit value: ", limit_value)
     if current_sum >= limit_value:
         print(player_2, " wins")
         break
     player_2_num = int(input(player_2 + ": "))
     current_sum += player_2_num
-    print("Current sum: ",current_sum)
+    print("Current sum: ", current_sum, " Limit value: ", limit_value)
     if current_sum >= limit_value:
         print(player_1, " wins")
         break
