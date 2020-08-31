896f25280cd23c50be6e147afa667ff07a287c4e
Prime Patel <primespatel@gmail.com>
2020-08-31 21:57:30 +0530

    n player version

--- main.py
+++ main.py
@@ -6,21 +6,6 @@ class Player:
         self.name = name
         self.color = color
 
-
-print("welcome")
-
-COLORS = ["\u001b[31;1m", "\u001b[32m", "\u001b[33;1m", "\u001b[34;1m", "\u001b[35m", "\u001b[36m", "\u001b[37m"]
-
-def valid_name(prompt):
-    name = input(prompt)
-    while name == "":
-        name = input(prompt)
-    return name
-
-PLAYERS = []
-for i in range(2):
-    PLAYERS.append(Player(valid_name("Player 1 Name: "), random.choice(COLORS)))
-
 def input_num(prompt):
     num = input(prompt)
     for i in range(9):
@@ -36,6 +21,22 @@ def input_num(prompt):
         print("Maximum number of error exceeded")
         exit()
     return num
+
+print("welcome")
+
+COLORS = ["\u001b[31;1m", "\u001b[32m", "\u001b[33;1m", "\u001b[34;1m", "\u001b[35m", "\u001b[36m", "\u001b[37m"]
+
+def valid_name(prompt):
+    name = input(prompt)
+    while name == "":
+        name = input(prompt)
+    return name
+
+n = input_num("No. of Players: ")
+
+PLAYERS = []
+for i in range(n):
+    PLAYERS.append(Player(valid_name(f"Player {i} Name: "), random.choice(COLORS)))
 
 limit_value = input_num("Limit Value: ")
 
@@ -56,15 +57,13 @@ def valid_num(player):
 current_sum = 0
 
 while current_sum<limit_value:
-    num = valid_num(PLAYERS[0].color + PLAYERS[0].name)
-    current_sum += num
-    print("Current sum: ", current_sum, " Limit value: ", limit_value)
-    if current_sum >= limit_value:
-        print(PLAYERS[1].name, " wins")
-        break
-    num = valid_num(PLAYERS[1].color + PLAYERS[1].name)
-    current_sum += num
-    print("Current sum: ", current_sum, " Limit value: ", limit_value)
-    if current_sum >= limit_value:
-        print(PLAYERS[0].name, " wins")
-        break
+    for player in PLAYERS:
+        num = valid_num(player.color + player.name)
+        current_sum += num
+        print("Current sum: ", current_sum, " Limit value: ", limit_value)
+        if current_sum >= limit_value:
+            PLAYERS.remove(player)
+            print("WINNER(S): ", end=" ")
+            for other_player in PLAYERS:
+                print(other_player.name, end=" ")
+            break
