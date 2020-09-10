0473ce22419bf974d9b011450c7be87be0259fcf
Prime Patel <primespatel@gmail.com>
2020-09-10 22:34:37 +0530

    Game class introduced

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import random
 
-
-class Player:
-    def __init__(self, name, color) -> None:
-        self.name = name
-        self.color = color
-
 def input_num(prompt):
     num = input(prompt)
     for i in range(9):
@@ -22,6 +16,15 @@ def input_num(prompt):
         exit()
     return num
 
+def valid_num(player):
+    prompt = player + ": "
+    num = input_num(prompt)
+    while 0 >= num or num > max_value:
+        print("Invalid Number")
+        num = input_num(prompt)
+    return num
+
+
 print("welcome")
 
 COLORS = ["\u001b[31;1m", "\u001b[32m", "\u001b[33;1m", "\u001b[34;1m", "\u001b[35m", "\u001b[36m", "\u001b[37m"]
@@ -31,6 +34,31 @@ def valid_name(prompt):
     while name == "":
         name = input(prompt)
     return name
+
+class Player:
+    def __init__(self, name, color) -> None:
+        self.name = name
+        self.color = color
+
+class Game:
+    def __init__(self, players, limit, max_entry) -> None:
+        self.PLAYERS = players
+        self.limit = limit
+        self.maxentry = max_entry
+    def play(self):
+        current_sum = 0
+        while current_sum<self.limit:
+            for player in self.PLAYERS:
+                num = valid_num(player.color + player.name)
+                current_sum += num
+                print("Current sum: ", current_sum, " Limit value: ", limit_value)
+                if current_sum >= limit_value:
+                    PLAYERS.remove(player)
+                    print("WINNER(S): ", end=" ")
+                    for other_player in PLAYERS:
+                        print(other_player.name, end=" ")
+                    break
+
 
 n = input_num("No. of Players: ")
 
@@ -46,24 +74,7 @@ max_value = input_num("Max value: ")
 
 print("Max Value is ", max_value)
 
-def valid_num(player):
-    prompt = player + ": "
-    num = input_num(prompt)
-    while 0 >= num or num > max_value:
-        print("Invalid Number")
-        num = input_num(prompt)
-    return num
+game = Game(PLAYERS,limit_value, max_value)
 
-current_sum = 0
-
-while current_sum<limit_value:
-    for player in PLAYERS:
-        num = valid_num(player.color + player.name)
-        current_sum += num
-        print("Current sum: ", current_sum, " Limit value: ", limit_value)
-        if current_sum >= limit_value:
-            PLAYERS.remove(player)
-            print("WINNER(S): ", end=" ")
-            for other_player in PLAYERS:
-                print(other_player.name, end=" ")
-            break
+while len(game.PLAYERS) != 1:
+    game.play()
